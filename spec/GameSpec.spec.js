74d863e09cf6b9dc1d159d107fea2d06308ec38b
WeenieMelon <manuelsurkc@gmail.com>
2024-04-25 11:44:00 -0500

    Added failures on purpose and added more specific test cases

--- spec/GameSpec.spec.js
+++ spec/GameSpec.spec.js
@@ -1,45 +1,46 @@
-describe("Calculator", function() {
-  beforeEach(function(){
-    var fixture = '<div id="container">'+
-    '<div id="calculator">'+
-      '<div id="result">'+
-        '<div id="history">'+
-          '<p id="history-value"></p>'+
-        '</div>'+
-        '<div id="output">'+
-          '<p id="output-value"></p>'+
-        '</div>'+
+beforeEach(function(){
+  var fixture = '<div id="container">'+
+  '<div id="calculator">'+
+    '<div id="result">'+
+      '<div id="history">'+
+        '<p id="history-value"></p>'+
       '</div>'+
-      '<div id="keyboard">'+
-        '<button class="operator" id="clear">C</button>'+
-        '<button class="operator" id="backspace">CE</button>'+
-        '<button class="operator" id="%">%</button>'+
-        '<button class="operator" id="/">&#247;</button>'+
-        '<button class="number" id="7">7</button>'+
-        '<button class="number" id="8">8</button>'+
-        '<button class="number" id="9">9</button>'+
-        '<button class="operator" id="*">&times;</button>'+
-        '<button class="number" id="4">4</button>'+
-        '<button class="number" id="5">5</button>'+
-        '<button class="number" id="6">6</button>'+
-        '<button class="operator" id="-">-</button>'+
-        '<button class="number" id="1">1</button>'+
-        '<button class="number" id="2">2</button>'+
-        '<button class="number" id="3">3</button>'+
-        '<button class="operator" id="+">+</button>'+
-        '<button class="empty" id="empty"></button>'+
-        '<button class="number" id="0">0</button>'+
-        '<button class="empty" id="empty"></button>'+
-        '<button class="operator" id="=">=</button>'+
+      '<div id="output">'+
+        '<p id="output-value"></p>'+
       '</div>'+
     '</div>'+
-  '</div>';
-  document.body.insertAdjacentHTML(
-    'afterbegin',fixture);
-  })
-  afterEach(function(){
-    document.body.removeChild(document.getElementById('container'));
-  })
+    '<div id="keyboard">'+
+      '<button class="operator" id="clear">C</button>'+
+      '<button class="operator" id="backspace">CE</button>'+
+      '<button class="operator" id="%">%</button>'+
+      '<button class="operator" id="/">&#247;</button>'+
+      '<button class="number" id="7">7</button>'+
+      '<button class="number" id="8">8</button>'+
+      '<button class="number" id="9">9</button>'+
+      '<button class="operator" id="*">&times;</button>'+
+      '<button class="number" id="4">4</button>'+
+      '<button class="number" id="5">5</button>'+
+      '<button class="number" id="6">6</button>'+
+      '<button class="operator" id="-">-</button>'+
+      '<button class="number" id="1">1</button>'+
+      '<button class="number" id="2">2</button>'+
+      '<button class="number" id="3">3</button>'+
+      '<button class="operator" id="+">+</button>'+
+      '<button class="empty" id="empty"></button>'+
+      '<button class="number" id="0">0</button>'+
+      '<button class="empty" id="empty"></button>'+
+      '<button class="operator" id="=">=</button>'+
+    '</div>'+
+  '</div>'+
+'</div>';
+document.body.insertAdjacentHTML(
+  'afterbegin',fixture);
+})
+afterEach(function(){
+  document.body.removeChild(document.getElementById('container'));
+})
+describe("Calculator", function() {
+  
   it("should return history value", function() {
     document.getElementById("history-value").innerText = "5+";
     expect(getHistory()).toEqual("5+");
@@ -66,7 +67,9 @@ describe("Calculator", function() {
   it("should reverse formatted number correctly", function() {
     expect(reverseNumberFormat("1,000")).toEqual(1000);
   });
-  it("should add two numbers correctly", function() {
+});
+describe("Calculator operations",function(){
+  it("should add two numbers correctly (5+5 = 10)", function() {
     var history= "5+";
     var output ="5";
     history = history + output;
@@ -78,7 +81,7 @@ describe("Calculator", function() {
     document.getElementById("=").click();
     expect(document.getElementById("output-value").innerText).toEqual("10");
   });
-  it("should subtract two numbers correctly", function() {
+  it("should subtract two numbers correctly (10-5 = 5)", function() {
     var history= "10-";
     var output ="5";
     history = history + output;
@@ -90,7 +93,7 @@ describe("Calculator", function() {
     document.getElementById("=").click();
     expect(document.getElementById("output-value").innerText).toEqual("5");
   });
-  it("should multiply two numbers correctly", function() {
+  it("should multiply two numbers correctly(5*5 = 25)", function() {
     var history= "5*";
     var output ="5";
     history = history + output;
@@ -102,7 +105,7 @@ describe("Calculator", function() {
     document.getElementById("=").click();
     expect(document.getElementById("output-value").innerText).toEqual("25");
   });
-  it("should divide two numbers correctly", function() {
+  it("should divide two numbers correctly (10/5 = 2)", function() {
     var history= "10/";
     var output ="5";
     history = history + output;
@@ -114,7 +117,7 @@ describe("Calculator", function() {
     document.getElementById("=").click();
     expect(document.getElementById("output-value").innerText).toEqual("2");
   });
-  it("should divide two numbers correctly", function() {
+  it("should mod two numbers correctly (9%2 = 1)", function() {
     var history= "9%";
     var output ="2";
     history = history + output;
@@ -126,6 +129,23 @@ describe("Calculator", function() {
     document.getElementById("=").click();
     expect(document.getElementById("output-value").innerText).toEqual("1");
   });
+})
+describe("Calculator failures", function(){
+  it("This is what a failure looks like",function(){
+    expect(true).toEqual(false);
+  })
+  it("should add two number INCORRECTLY (5+5 = 15)", function() {
+    var history= "5+";
+    var output ="5";
+    history = history + output;
+    printHistory("5+");
+    printOutput("5");
+    var result = eval(history);
+    document.getElementById("+").click();
+    printOutput(result);
+    document.getElementById("=").click();
+    expect(document.getElementById("output-value").innerText).toEqual("15");
+  });
 
-});
+})
   
